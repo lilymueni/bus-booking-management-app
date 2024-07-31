ec7fb3a7c30dfd35a73fb188c1c79a6b0f774e14
kipkemoi Haron <kipkemoiharon4@gmail.com>
2024-07-31 16:11:20 +0300

    Add Contact Us button and form

---
+++ src/App.js/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ContactUsButton from './components/ContactUsButton';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Bus Management System</h1>
+            <ContactUsButton />
+            {/* Add other components here */}
+        </div>
+    );
+}
+
+export default App;
