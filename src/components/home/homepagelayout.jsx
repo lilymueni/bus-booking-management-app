794288c394c53baf302880ee9f9110f0041e0fb2
lilian <lilliannzioki@gmail.com>
2024-08-01 18:11:47 +0300

    Your commit message

--- src/components/home/homepagelayout.jsx
+++ src/components/home/homepagelayout.jsx
@@ -17,13 +17,9 @@ const HomePageLayout = () => {
         <div className="hero-content">
           <img src="https://www.getbus.org/wp-content/uploads/2024/05/Website-banner9.jpg" alt="Bus" className="hero-image" />
           <h1>Welcome to TransitWise</h1>
-          <p>Your journey starts here. Book, Pay, Travel with ease.</p>
+          <p>Your journey starts here. Book,Pay, Travel with ease.</p>
         </div>
       </header>
-      <section className="about-us">
-        <h2>About Us</h2>
-        <p>TransitWise is committed to making your travel experience seamless and enjoyable. We offer a range of services to help you book, pay, and travel with ease. Our platform is designed with your convenience in mind, ensuring that you have the best journey possible. Learn more about our features and how we can help you get where you need to go.</p>
-      </section>
       <section className="booking-form">
         <h2>Book, Pay, Travel</h2>
         <form>
@@ -46,6 +42,11 @@ const HomePageLayout = () => {
           <button type="submit">Search</button>
         </form>
       </section>
+      <section className="about-us">
+        <h2>About Us</h2>
+        <p>TransitWise is committed to making your travel experience seamless and enjoyable. We offer a range of services to help you book, pay, and travel with ease. Our platform is designed with your convenience in mind, ensuring that you have the best journey possible. Learn more about our features and how we can help you get where you need to go.</p>
+      </section>
+      
       <section className="reasons">
         <div className="card">
           <h3>Reason 1</h3>
