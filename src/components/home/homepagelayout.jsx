558545c7a55a4d908f4c28e3f24b713cd9b7ecee
lilian <lilliannzioki@gmail.com>
2024-08-06 23:00:03 +0300

    update customer-dashboard

--- src/components/home/homepagelayout.jsx
+++ src/components/home/homepagelayout.jsx
@@ -14,35 +14,16 @@ const HomePageLayout = () => {
       </section>
       <header className="hero">
         <div className="hero-content">
-          <img src="https://www.getbus.org/wp-content/uploads/2024/05/Website-banner9.jpg" alt="Bus" className="hero-image" />
+          <img src="https://media.istockphoto.com/id/1472071952/vector/electric-city-bus-charging-station-with-green-city-city-building-in-background-vector.jpg?s=612x612&w=0&k=20&c=mpWPr_3_9YAlQIQ4EArllVY910o9lb3yMtbEUo7kcyo=" />
         </div>
       </header>
-      <section className="booking-form">
-        <h1> Hasssle-Free Booking...</h1>
-        <h2>Book, Pay, Travel</h2>
-        <form>
-          <label>
-            From:
-            <input type="text" name="from" placeholder="Starting Point" />
-          </label>
-          <label>
-            To:
-            <input type="text" name="to" placeholder="Destination" />
-          </label>
-          <label>
-            Date:
-            <input type="date" name="date" />
-          </label>
-          <label>
-            Search:
-            <input type="text" name="search" placeholder="Search Buses" />
-          </label>
-          <button type="submit">Search</button>
-        </form>
-      </section>
+     
       
-      <div>
+      <div className="centered-text">
   <h2>Your Trusted Travel Partner!</h2>
+
+
+    
   <section className="reasons">
     <div className="card">
       <br /><br />
@@ -79,9 +60,33 @@ const HomePageLayout = () => {
       Experience premium comfort and top-notch service on every trip.</p>
     </div>
   </section>
+  {/* <section className="booking-form">
+  <h1> Hasssle-Free Booking...</h1>
+  <h2>Book, Pay, Travel</h2>
+  <form>
+    <label>
+      From:
+      <input type="text" name="from" placeholder="Starting Point" />
+    </label>
+    <label>
+      To:
+      <input type="text" name="to" placeholder="Destination" />
+    </label>
+    <label>
+      Date:
+      <input type="date" name="date" />
+    </label>
+    <label>
+      Search:
+      <input type="text" name="search" placeholder="Search Buses" />
+    </label>
+    <button type="submit">Search</button>
+  </form>
+</section> */}
+
   <section className="about-us">
         <h2>About Us</h2>
-        <p>TransitWise is committed to making your travel experience seamless and enjoyable. We offer a range of services to help you book, pay, and travel with ease. Our platform is designed with your convenience in mind, ensuring that you have the best journey possible. Learn more about our features and how we can help you get where you need to go.</p>
+        <p>TransitWise is committed to making your travel experience seamless and enjoyable. We offer a range of services to help you book, pay, and travel with ease. Our platform is designed with your convenience in mind, ensuring that you have the best journey possible. Learn more about our features and how we can help you get where you need to go. We strive to provide exceptional customer service and support, ensuring your needs are met every step of the way. Our user-friendly interface makes it easy to plan and manage your trips efficiently. With TransitWise, you can travel with confidence, knowing that we prioritize your safety and comfort.</p>
       </section>
       
 </div>
